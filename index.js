b554751683617723cefeca852a00f5a0a0ccb001
Daniela Rodríguez Careri <dcareri@gmail.com>
2017-06-18 17:26:27 -0300

    Implement the needed strategies for the FJS project

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-/*
+/**
  * Build sound attachments according to ATTR_manip_* commands
- * Author: Daniela Rodriguez Careri <dcareri@gmail.com>
+ * @author Daniela Rodriguez Careri <dcareri@gmail.com>
+ * @todo param to ignore datarefs
+ * @todo param to ignore lines
+ *
  */
 
 const
@@ -51,33 +54,89 @@ const eventsByCursorType = {
 	'arrow': events.SWITCH
 };
 
+/**
+ * @param params
+ * @returns {string}
+ */
 function commandStrategy(params) {
 	let event = eventsByCursorType[params[0]],
-		dref = params[1];
+		command = params[1];
 
-	return '';
-	console.log(params);
-	// return 'SARASA';
+	return "\tEVENT_NAME " + event + "\r\n" +
+		   "\tEVENT_CMND_DOWN " + command + "\r\n";
+}
+
+/**
+ * @param params
+ * @returns {string}
+ */
+function pushStrategy(params) {
+	let event = eventsByCursorType[params[0]],
+		v_down = parseFloat(params[1]),
+		v_up = parseFloat(params[2]),
+		dref = params[3];
+
+	return "\tEVENT_NAME " + event + "\r\n" +
+		"\tEVENT_START_COND " + dref + " = " + v_down +  "\r\n" +
+		"\tEVENT_END_COND " + dref + " = " + v_up +  "\r\n" // TODO: This could be optional.
+	;
+}
+
+/**
+ * @param params
+ * @returns {string}
+ */
+function toggleStrategy(params) {
+	let event = eventsByCursorType[params[0]],
+		v_on = params[1],
+		v_off = params[2],
+		dref = params[3];
+
+	return "\tEVENT_NAME " + event + "\r\n" +
+		"\tEVENT_START_COND ABS_DELTA=" + dref + " > 0\r\n" +
+		"\tEVENT_END_COND ABS_DELTA=" + dref + " = 0\r\n" // TODO: this could be optional
+		;
+}
+
+/**
+ * @param params
+ * @returns {string}
+ */
+function deltaStrategy(params) {
+	let event = eventsByCursorType[params[0]],
+		v_down = parseFloat(params[1]),
+		v_hold = params[2],
+		v_min = params[3],
+		v_max = params[4],
+		dref = params[5]
+	;
+
+	return "\tEVENT_NAME " + event + "\r\n" +
+		"\tEVENT_START_COND DELTA=" + dref + (v_down > 0 ? " >= " + v_down : " <= " + v_down ) + "\r\n" +
+		"\tEVENT_END_COND DELTA=" + dref + " = 0\r\n"
+		;
 }
 
 /**
  * Which strategy function should be used with which manipulator type
+ * @see http://developer.x-plane.com/?article=obj8-file-format-specification
+ * @see http://developer.x-plane.com/?article=sound-snd-file-format-specification
  * @type {object}
  */
 const strategiesByManipType = {
 	'ATTR_manip_command': commandStrategy,
-	// 'ATTR_manip_command_axis': commandStrategy,
-	// 'ATTR_manip_push': commandStrategy,
-	// 'ATTR_manip_toggle': commandStrategy,
-	// 'ATTR_manip_delta': commandStrategy,
-	// 'ATTR_manip_wrap': commandStrategy,
-	// 'ATTR_manip_wheel': commandStrategy,
-	// 'ATTR_manip_command_knob': commandStrategy,
-	// 'ATTR_manip_switch_up_down': commandStrategy,
-	// 'ATTR_manip_switch_left_right': commandStrategy,
-	// 'ATTR_manip_axis_knob': commandStrategy,
-	// 'ATTR_manip_axis_switch_up_down': commandStrategy,
-	// 'ATTR_manip_axis_switch_left_right': commandStrategy,
+	// 'ATTR_manip_command_axis': function() {}, // TODO
+	'ATTR_manip_push': pushStrategy,
+	'ATTR_manip_toggle': toggleStrategy,
+	'ATTR_manip_delta': deltaStrategy,
+	'ATTR_manip_wrap': deltaStrategy,
+	// 'ATTR_manip_wheel': function() {}, // TODO, requires knowledge of the previous command
+	// 'ATTR_manip_command_knob': function() {}, // TODO
+	// 'ATTR_manip_switch_up_down': function() {}, // TODO
+	// 'ATTR_manip_switch_left_right': function() {}, // TODO
+	// 'ATTR_manip_axis_knob': function() {}, // TODO
+	// 'ATTR_manip_axis_switch_up_down': function() {}, // TODO
+	// 'ATTR_manip_axis_switch_left_right': function() {}, // TODO
 };
 
 /**
@@ -93,7 +152,7 @@ function getManips(filename) {
 		l = l.trim();
 		if (l.includes('ATTR_manip')) {
 			ret.push({
-				idx: i,
+				idx: i + 1,
 				text: l
 			});
 		}
@@ -102,11 +161,23 @@ function getManips(filename) {
 }
 
 /**
- * Build attachment for a single line
+ * Creates the text structure
+ * @param content
+ * @returns {string}
+ */
+function buildAttachmentDefinition(content) {
+	return "BEGIN_SOUND_ATTACHMENT\r\n" +
+	"\tVEH_PART cockpit 0\r\n" +
+	content +
+	"END_SOUND_ATTACHMENT\r\n";
+}
+
+/**
+ * Build attachments for a manipulator
  * @param line
  * @returns {*}
  */
-function buildAttachment(line) {
+function buildAttachmentsForManip(line) {
 	let
 		ret = null,
 		parts = line.text.split(/\s+/g),
@@ -114,12 +185,8 @@ function buildAttachment(line) {
 		strategyFn = strategiesByManipType[manipType]
 	;
 	if (strategyFn) {
-		ret = `# ${filename} line ${line.idx}
-BEGIN_SOUND_ATTACHMENT
-	VEH_PART cockpit 0
-${strategyFn(parts.slice(1))}
-END_SOUND_ATTACHMENT
-`;
+		ret = "# " + filename + " line " + line.idx + " (" + manipType + " " + parts[1] + ")\r\n" +
+			buildAttachmentDefinition(strategyFn(parts.slice(1)))
 	}
 	return ret;
 }
@@ -137,11 +204,10 @@ Commander
 let manips = getManips(filename);
 console.log(`# Read ${manips.length} manipulators from file ${filename}.`);
 // manips = manips.slice(0,5); // fixme
-let attachments = manips.map(line => buildAttachment(line)).filter(att => {
+let attachments = manips.map(manip => buildAttachmentsForManip(manip)).filter(att => {
 	return att !== null
 });
 console.log(`# Built ${attachments.length} attachments.`);
 console.log();
-console.log(attachments.forEach(att => console.log(att)));
-
+attachments.forEach(att => console.log(att));
 process.exit(1);
